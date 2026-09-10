10558a486d7a39aeeb40efad97c53c2fe3fa1317
ocmila1708-jpg <ocmila1708@gmail.com>
2026-09-10 02:07:38 +0000

    creacion de directivas

--- BuscadorPokemon/src/app/services/pokemon-storage.ts
+++ BuscadorPokemon/src/app/services/pokemon-storage.ts
@@ -34,4 +34,37 @@ export class PokemonStorageService {
       this.misPokemones.set(JSON.parse(data));
     }
   }
+  //-1. Obtener datos de la API 
+  buscarEnApi(nombreOId: string) {
+    return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${nombreOId.toLowerCase()}`);
+
+  }
+  // -2.Guardar/crear  pokemon dentro del el maleto
+
+  guardarPokemon(nuevo: pokemonTarjeta) {
+    const actualizados = [...this.misPokemones(), nuevo];
+    this.misPokemones.set(actualizados);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(actualizados));
+  }
+
+  // -3. Actualizar Pokemon Favorito.
+
+  actualizarFavorito(id: number) {
+    const actualizados = this.misPokemones().map(poke => {
+      if (poke.id === id) {
+        return { ...poke, esFavorito: !poke.esFavorito};
+      }
+      return poke;
+    });
+    this.misPokemones.set(actualizados);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(actualizados));
+      
+  };
+
+  //-4. Eliminar Pokemon del maleto
+  eliminarPokemon(id: number) {
+    const filtrados = this.misPokemones().filter(poke => poke.id !== id);
+    this.misPokemones.set(filtrados);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(filtrados));
+  }
 }
